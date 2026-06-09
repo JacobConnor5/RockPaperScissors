b46d19283340f96b026085c657300b886f1bf506
JacobConnor <gn24034@bristol.ac.uk>
2026-06-09 20:32:11 +0100

    Temporary stuff

--- test.py
+++ test.py
@@ -15,37 +15,7 @@ FONT_SIZE = 1
 FONT_THICKNESS = 1
 HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-  hand_landmarks_list = detection_result.hand_landmarks
-  handedness_list = detection_result.handedness
-  annotated_image = np.copy(rgb_image)
-
-  # Loop through the detected hands to visualize.
-  for idx in range(len(hand_landmarks_list)):
-    hand_landmarks = hand_landmarks_list[idx]
-    handedness = handedness_list[idx]
-
-    # Draw the hand landmarks.
-    mp_drawing.draw_landmarks(
-      annotated_image,
-      hand_landmarks,
-      mp_hands.HAND_CONNECTIONS,
-      mp_drawing_styles.get_default_hand_landmarks_style(),
-      mp_drawing_styles.get_default_hand_connections_style())
-
-    # Get the top left corner of the detected hand's bounding box.
-    height, width, _ = annotated_image.shape
-    x_coordinates = [landmark.x for landmark in hand_landmarks]
-    y_coordinates = [landmark.y for landmark in hand_landmarks]
-    text_x = int(min(x_coordinates) * width)
-    text_y = int(min(y_coordinates) * height) - MARGIN
-
-    # Draw handedness (left or right hand) on the image.
-    cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
-
-  return annotated_image
+ 
 # STEP 2: Create an HandLandmarker object.
 base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
 options = vision.HandLandmarkerOptions(base_options=base_options,
@@ -60,9 +30,3 @@ detection_result = detector.detect(image)
 print(detection_result)
 # STEP 5: Process the classification result. In this case, visualize it.
 annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-
-while True:
-  cv2.imshow("myImage",annotated_image)
-
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
---
+++ train.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import os
+import sklearn,pickle
+from skimage.transform import resize
+from skimage.io import imread
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+# STEP 1: Import the necessary modules.
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import numpy as np
+import time
+import main
+
+mp_hands = mp.tasks.vision.HandLandmarksConnections
+mp_drawing = mp.tasks.vision.drawing_utils
+mp_drawing_styles = mp.tasks.vision.drawing_styles
+
+
+
+Categories=['Rock','Paper','Scissors']
+
+dataArr=[] #input array will have colour of each pixelin the array
+target_arr=[] #output array will have the answer so thsese colours mean rock
+datadir=''
+#path which contains all the categories of images
+
+
+for i in Categories:
+    
+    print(f'loading... category : {i}')
+    path=os.path.join(datadir,i)
+    
+    for img in os.listdir(path):
+        image = mp.Image.create_from_file(os.path.join(path,img))
+        #img_resized=resize(img_array,(150,150,3))
+
+        detection_result = main.detector.detect(image)
+        #print(annotated_image)
+        #Fall back to the raw image if no landmarks detected
+        print(os.path.join(path,img))
+        raw_numpy = image.numpy_view().copy()
+        
+        if detection_result.hand_landmarks:  # or face_landmarks / pose_landmarks depending on your model
+            annotated_image = main.draw_landmarks_on_image(raw_numpy, detection_result)
+        else:
+            annotated_image = raw_numpy  # use original if nothing detected
+        
+        resizedImg = cv2.resize(annotated_image, (150, 150))
+        dataArr.append(resizedImg) 
+
+        # dataArr.append(annotated_image.tolist())
+        target_arr.append(Categories.index(i))
+    print(f'loaded category:{i} successfully')
+
+print(type(dataArr))
+feat = np.array(dataArr)
+label=np.array(target_arr)
+print(label)
+
+feat_train,feat_test,label_train,label_test = sklearn.model_selection.train_test_split(feat,label,test_size = 0.1)
+
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(feat_train,label_train)
+acc = model.score(feat_test,label_test)
+predictions = model.predict(feat_test)
+
+for i in range(len(predictions)):
+    print('prediction:',predictions[i],'actual:',label_test[i])
+
+print(acc)
+
+with open('Rock.pickle','wb') as pickle_file:
+    pickle.dump(model,pickle_file)
+    pickle_file.close()
+
